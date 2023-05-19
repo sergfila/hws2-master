ae848ddd5dcadaf4bf066f676ccfd710f1f087e5
sergfila <sergfila@live.ru>
2023-05-19 21:34:11 +0300

    completed 8 HW

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,16 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
 
-            return state // need to fix
+            return state.sort((a, b) => action.payload === 'up'
+                ? b.age - a.age : a.age - b.age)
         }
         case 'check': {
 
-            return state // need to fix
+            return state.filter(el => el.age >= action.payload)
         }
         default:
             return state
